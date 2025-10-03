650248b05c7e65962401909dae9d8d444a91e14b
annilq <410493517@qq.com>
2025-10-03 10:07:31 +0800

    1. use prerender when add auth support

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,8 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prismaClientSingleton = () => {
-    return new PrismaClient()
-}
+const prisma = new PrismaClient({
+  datasourceUrl: import.meta.env.DATABASE_URL,
+}).$extends(withAccelerate());
 
-declare global {
-    var prisma: ReturnType<typeof prismaClientSingleton>
-}
-
-const prisma = globalThis.prisma ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+export default prisma;
